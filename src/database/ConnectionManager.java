772e3dda9fd89e74737e0187bfbe23f72aada3d2
Rafael Cosio Castro <32781770+r-cosiocastro@users.noreply.github.com>
2022-11-23 02:43:19 -0600

    Update ConnectionManager.java

--- src/database/ConnectionManager.java
+++ src/database/ConnectionManager.java
@@ -19,7 +19,7 @@ public class ConnectionManager {
     private static final String URL = "jdbc:mysql://localhost:3306/bannet?useSSL=false&serverTimezone=UTC&autoReconnect=true";
     private static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "krawzer1259089";
+    private static final String PASSWORD = "root";
     private static Connection connection;
 
     public static Connection getConnection() throws SQLException {
